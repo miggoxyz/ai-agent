c56ebf1346f4d23f72c6b1c9fe27960ebde3032a
Ioakeim Mingo <canonshai@gmail.com>
2025-06-16 14:21:48 +0100

    Add gitignore Add requirements Basic implementation of accepting prompt and sending to gemini api

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    args = sys.argv[1:]
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=user_prompt
+    )
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()

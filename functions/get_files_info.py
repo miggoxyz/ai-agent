5dc3d2dfcd8983488f650b4ea1262ade96e521c7
Ioakeim Mingo <canonshai@gmail.com>
2025-06-22 22:01:33 +0100

    get info files function declaration make available to ai

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 
 def get_files_info(working_directory, directory=None):
@@ -12,3 +13,18 @@ def get_files_info(working_directory, directory=None):
     items = [
         f"- {item}: file_size={os.path.getsize(os.path.join(directory, item))} bytes, is_dir={os.path.isdir(os.path.join(directory, item))}" for item in os.listdir(directory)]
     return "\n".join(items)
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 from config import SYSTEM_PROMPT
+from call_function import available_functions
 
 
 def main():
@@ -37,13 +38,18 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT)
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=SYSTEM_PROMPT)
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+    if not response.function_calls:
+        print(response.text)
+        return response.text
+    for function_call_part in response.function_calls:
+        print(
+            f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 if __name__ == "__main__":

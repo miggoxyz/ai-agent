bfea8c82415e5a660dad5834739db66b7411e4f9
Ioakeim Mingo <canonshai@gmail.com>
2025-06-23 13:01:32 +0100

    implement the calling function functionality

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 from config import SYSTEM_PROMPT
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 
 def main():
@@ -47,9 +47,21 @@ def generate_content(client, messages, verbose):
     if not response.function_calls:
         print(response.text)
         return response.text
+    function_responses = []
     for function_call_part in response.function_calls:
-        print(
-            f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(
+                f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
 
 
 if __name__ == "__main__":
